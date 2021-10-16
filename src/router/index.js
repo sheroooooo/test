c8ab33a544dd5687f30232e34528d061cbb12284
18832126308@163.com <18832126308@163.com>
2021-10-16 15:06:27 +0800

    配置登录页路由，引入elementui

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,33 @@
+// import {createRouter, createWebHashHistory} from "vue-router"
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
 Vue.use(Router)
-
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {
+      title: '登录'
+    },
+    component: () => import(/* webpackChunkName: "login" */ '../view/Login.vue')
+  }
+]
+const router = new Router({
+  routes,
+  mode: 'history'
 })
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | vue-manage-system`
+  const role = localStorage.getItem('ms_username')
+  if (!role && to.path !== '/login') {
+    next('/login')
+  } else if (to.meta.permission) {
+    // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
+    role === 'admin'
+      ? next()
+      : next('/403')
+  } else {
+    next()
+  }
+})
+export default router
---
+++ src/view/login.vue
@@ -0,0 +1,7 @@
+<template>
+
+  <div>
+	<p>11</p>
+  </div>
+
+</template>
